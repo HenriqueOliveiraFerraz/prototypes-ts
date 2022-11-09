29eab02ca9594e3b541b80d82e54fad0aab185a3
Henrique Ferraz <henrique.oliveira.ferraz@protonmail.com>
2022-11-09 08:42:23 -0300

    test: check grouped type

--- src/__tests__/array.prototypes.spec.ts
+++ src/__tests__/array.prototypes.spec.ts
@@ -3,88 +3,128 @@ import '../array-prototypes/array.prototypes';
 describe('groupBy', () => {
   type Inventory = {
     name: string;
-    type: string;
+    type: InventoryTypes;
     quantity: number;
+    address: { state: string; country: string };
   };
 
+  type InventoryTypes = 'vegetables' | 'fruit' | 'meat';
+  type InventoryStates = 'CA' | 'TX' | 'AL';
+
+  type GroupedByInventoryTypes = Record<InventoryTypes, Inventory[]>;
+  type GroupedByInventoryStates = Record<InventoryStates, Inventory[]>;
+
+  const inventory: Inventory[] = [
+    { name: 'asparagus', type: 'vegetables', quantity: 5, address: { state: 'CA', country: 'USA' } },
+    { name: 'bananas', type: 'fruit', quantity: 0, address: { state: 'TX', country: 'USA' } },
+    { name: 'goat', type: 'meat', quantity: 23, address: { state: 'AL', country: 'USA' } },
+    { name: 'cherries', type: 'fruit', quantity: 5, address: { state: 'TX', country: 'USA' } },
+    { name: 'fish', type: 'meat', quantity: 22, address: { state: 'CA', country: 'USA' } }
+  ];
+
   test('should groupBy inventory type', () => {
-    const inventory: Inventory[] = [
-      { name: 'asparagus', type: 'vegetables', quantity: 5 },
-      { name: 'bananas', type: 'fruit', quantity: 0 },
-      { name: 'goat', type: 'meat', quantity: 23 },
-      { name: 'cherries', type: 'fruit', quantity: 5 },
-      { name: 'fish', type: 'meat', quantity: 22 }
-    ];
-
     const groupedByType = inventory.groupBy((g) => g.type);
+    const expectedGroup: GroupedByInventoryTypes = {
+      vegetables: [{ name: 'asparagus', type: 'vegetables', quantity: 5, address: { state: 'CA', country: 'USA' } }],
+      fruit: [
+        { name: 'bananas', type: 'fruit', quantity: 0, address: { state: 'TX', country: 'USA' } },
+        { name: 'cherries', type: 'fruit', quantity: 5, address: { state: 'TX', country: 'USA' } }
+      ],
+      meat: [
+        { name: 'goat', type: 'meat', quantity: 23, address: { state: 'AL', country: 'USA' } },
+        { name: 'fish', type: 'meat', quantity: 22, address: { state: 'CA', country: 'USA' } }
+      ]
+    };
 
-    expect(groupedByType.vegetables).toEqual([{ name: 'asparagus', type: 'vegetables', quantity: 5 }]);
-    expect(groupedByType.vegetables.length).toEqual(1);
+    expect(groupedByType).toStrictEqual(expectedGroup);
 
-    expect(groupedByType.fruit).toEqual([
-      { name: 'bananas', type: 'fruit', quantity: 0 },
-      { name: 'cherries', type: 'fruit', quantity: 5 }
-    ]);
-    expect(groupedByType.fruit.length).toEqual(2);
+    expect(groupedByType.vegetables).toEqual(expectedGroup.vegetables);
+    expect(groupedByType.vegetables.length).toEqual(expectedGroup.vegetables.length);
 
-    expect(groupedByType.meat).toEqual([
-      { name: 'goat', type: 'meat', quantity: 23 },
-      { name: 'fish', type: 'meat', quantity: 22 }
-    ]);
-    expect(groupedByType.meat.length).toEqual(2);
+    expect(groupedByType.fruit).toEqual(expectedGroup.fruit);
+    expect(groupedByType.fruit.length).toEqual(expectedGroup.fruit.length);
+
+    expect(groupedByType.meat).toEqual(expectedGroup.meat);
+    expect(groupedByType.meat.length).toEqual(expectedGroup.meat.length);
   });
 
-  test('should groupBy inventory quantity', () => {
-    const inventory: Inventory[] = [
-      { name: 'asparagus', type: 'vegetables', quantity: 5 },
-      { name: 'bananas', type: 'fruit', quantity: 0 },
-      { name: 'goat', type: 'meat', quantity: 23 },
-      { name: 'cherries', type: 'fruit', quantity: 5 },
-      { name: 'fish', type: 'meat', quantity: 22 }
-    ];
+  test('should groupBy inventory addresses', () => {
+    const groupedByType = inventory.groupBy((g) => g.address.state);
+    const expectedGroup: GroupedByInventoryStates = {
+      CA: [
+        { name: 'asparagus', type: 'vegetables', quantity: 5, address: { state: 'CA', country: 'USA' } },
+        { name: 'fish', type: 'meat', quantity: 22, address: { state: 'CA', country: 'USA' } }
+      ],
+      TX: [
+        { name: 'bananas', type: 'fruit', quantity: 0, address: { state: 'TX', country: 'USA' } },
+        { name: 'cherries', type: 'fruit', quantity: 5, address: { state: 'TX', country: 'USA' } }
+      ],
+      AL: [{ name: 'goat', type: 'meat', quantity: 23, address: { state: 'AL', country: 'USA' } }]
+    };
 
-    const groupedByType = inventory.groupBy((g) => g.quantity);
+    expect(groupedByType).toStrictEqual(expectedGroup);
 
-    expect(groupedByType[5]).toEqual([
-      { name: 'asparagus', type: 'vegetables', quantity: 5 },
-      { name: 'cherries', type: 'fruit', quantity: 5 }
-    ]);
-    expect(groupedByType[5].length).toEqual(2);
+    expect(groupedByType.CA).toEqual(expectedGroup.CA);
+    expect(groupedByType.CA.length).toEqual(expectedGroup.CA.length);
 
-    expect(groupedByType[0]).toEqual([{ name: 'bananas', type: 'fruit', quantity: 0 }]);
-    expect(groupedByType[0].length).toEqual(1);
+    expect(groupedByType.TX).toEqual(expectedGroup.TX);
+    expect(groupedByType.TX.length).toEqual(expectedGroup.TX.length);
 
-    expect(groupedByType[22]).toEqual([{ name: 'fish', type: 'meat', quantity: 22 }]);
-    expect(groupedByType[22].length).toEqual(1);
-
-    expect(groupedByType[23]).toEqual([{ name: 'goat', type: 'meat', quantity: 23 }]);
-    expect(groupedByType[23].length).toEqual(1);
+    expect(groupedByType.AL).toEqual(expectedGroup.AL);
+    expect(groupedByType.AL.length).toEqual(expectedGroup.AL.length);
   });
 
-  test('should groupBy inventory based on a condition with a custom string', () => {
-    const inventory: Inventory[] = [
-      { name: 'asparagus', type: 'vegetables', quantity: 5 },
-      { name: 'bananas', type: 'fruit', quantity: 0 },
-      { name: 'goat', type: 'meat', quantity: 23 },
-      { name: 'cherries', type: 'fruit', quantity: 5 },
-      { name: 'fish', type: 'meat', quantity: 22 }
-    ];
+  // test('should groupBy inventory quantity', () => {
+  //   const inventory: Inventory[] = [
+  //     { name: 'asparagus', type: 'vegetables', quantity: 5 },
+  //     { name: 'bananas', type: 'fruit', quantity: 0 },
+  //     { name: 'goat', type: 'meat', quantity: 23 },
+  //     { name: 'cherries', type: 'fruit', quantity: 5 },
+  //     { name: 'fish', type: 'meat', quantity: 22 }
+  //   ];
 
-    const groupedByType = inventory.groupBy((g) => {
-      return g.quantity > 5 ? 'ok' : 'restock';
-    });
+  //   const groupedByType = inventory.groupBy((g) => g.quantity);
 
-    expect(groupedByType.ok).toEqual([
-      { name: 'goat', type: 'meat', quantity: 23 },
-      { name: 'fish', type: 'meat', quantity: 22 }
-    ]);
-    expect(groupedByType.ok.length).toEqual(2);
+  //   expect(groupedByType[5]).toEqual([
+  //     { name: 'asparagus', type: 'vegetables', quantity: 5 },
+  //     { name: 'cherries', type: 'fruit', quantity: 5 }
+  //   ]);
+  //   expect(groupedByType[5].length).toEqual(2);
 
-    expect(groupedByType.restock).toEqual([
-      { name: 'asparagus', type: 'vegetables', quantity: 5 },
-      { name: 'bananas', type: 'fruit', quantity: 0 },
-      { name: 'cherries', type: 'fruit', quantity: 5 }
-    ]);
-    expect(groupedByType.restock.length).toEqual(3);
-  });
+  //   expect(groupedByType[0]).toEqual([{ name: 'bananas', type: 'fruit', quantity: 0 }]);
+  //   expect(groupedByType[0].length).toEqual(1);
+
+  //   expect(groupedByType[22]).toEqual([{ name: 'fish', type: 'meat', quantity: 22 }]);
+  //   expect(groupedByType[22].length).toEqual(1);
+
+  //   expect(groupedByType[23]).toEqual([{ name: 'goat', type: 'meat', quantity: 23 }]);
+  //   expect(groupedByType[23].length).toEqual(1);
+  // });
+
+  // test('should groupBy inventory based on a condition with a custom string', () => {
+  //   const inventory: Inventory[] = [
+  //     { name: 'asparagus', type: 'vegetables', quantity: 5 },
+  //     { name: 'bananas', type: 'fruit', quantity: 0 },
+  //     { name: 'goat', type: 'meat', quantity: 23 },
+  //     { name: 'cherries', type: 'fruit', quantity: 5 },
+  //     { name: 'fish', type: 'meat', quantity: 22 }
+  //   ];
+
+  //   const groupedByType = inventory.groupBy((g) => {
+  //     return g.quantity > 5 ? 'ok' : 'restock';
+  //   });
+
+  //   expect(groupedByType.ok).toEqual([
+  //     { name: 'goat', type: 'meat', quantity: 23 },
+  //     { name: 'fish', type: 'meat', quantity: 22 }
+  //   ]);
+  //   expect(groupedByType.ok.length).toEqual(2);
+
+  //   expect(groupedByType.restock).toEqual([
+  //     { name: 'asparagus', type: 'vegetables', quantity: 5 },
+  //     { name: 'bananas', type: 'fruit', quantity: 0 },
+  //     { name: 'cherries', type: 'fruit', quantity: 5 }
+  //   ]);
+  //   expect(groupedByType.restock.length).toEqual(3);
+  // });
 });
--- src/array-prototypes/array.prototypes.ts
+++ src/array-prototypes/array.prototypes.ts
@@ -1,17 +1,29 @@
 declare global {
   interface Array<T> {
-    groupBy<K extends keyof T>(callBack: (value: T, index: number) => T[K]): Record<PropertyKey, Array<T>>;
     groupBy(callBack: (value: T, index: number) => PropertyKey): Record<PropertyKey, Array<T>>;
+    //groupBy(callBack: (value: T, index: number) => T[K]): Record<T[K], Array<T>>;
   }
 }
 
-Array.prototype.groupBy = function <T, K extends keyof T>(callBack: (value: T, index: number) => T[K]) {
-  return this.reduce((previousValue, currentValue: T, currentIndex: number) => {
-    const key = callBack(currentValue, currentIndex);
-    const value: T[] = previousValue[key] || [];
-    previousValue[key] = value.concat([currentValue]);
-    return previousValue;
-  }, {} as Record<PropertyKey, T[]>);
+Array.prototype.groupBy = function <T>(callBack: (value: T, index: number) => PropertyKey) {
+  return this.reduce<Record<PropertyKey, Array<T>>>(
+    (previousValue: Record<PropertyKey, Array<T>>, currentValue: T, currentIndex: number) => {
+      const key = callBack(currentValue, currentIndex);
+      const value: T[] = previousValue[key] || [];
+      previousValue[key] = value.concat([currentValue]);
+      return previousValue;
+    },
+    {}
+  );
 };
+
+// Array.prototype.groupBy = function <T, K extends keyof T>(callBack: (value: T, index: number) => T[K]) {
+//   return this.reduce((previousValue, currentValue: T, currentIndex: number) => {
+//     const key = callBack(currentValue, currentIndex);
+//     const value: T[] = previousValue[key] || [];
+//     previousValue[key] = value.concat([currentValue]);
+//     return previousValue;
+//   }, {} as Record<PropertyKey, T[]>);
+// };
 
 export {};
