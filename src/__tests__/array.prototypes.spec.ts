709c2e4cc6355c54d8439c1413e1ed25bbdef9d3
Henrique Ferraz <henrique.oliveira.ferraz@protonmail.com>
2022-11-08 23:01:44 -0300

    feat: group by with custom key

--- src/__tests__/array.prototypes.spec.ts
+++ src/__tests__/array.prototypes.spec.ts
@@ -60,4 +60,31 @@ describe('groupBy', () => {
     expect(groupedByType[23]).toEqual([{ name: 'goat', type: 'meat', quantity: 23 }]);
     expect(groupedByType[23].length).toEqual(1);
   });
+
+  test('should groupBy inventory based on a condition with a custom string', () => {
+    const inventory: Inventory[] = [
+      { name: 'asparagus', type: 'vegetables', quantity: 5 },
+      { name: 'bananas', type: 'fruit', quantity: 0 },
+      { name: 'goat', type: 'meat', quantity: 23 },
+      { name: 'cherries', type: 'fruit', quantity: 5 },
+      { name: 'fish', type: 'meat', quantity: 22 }
+    ];
+
+    const groupedByType = inventory.groupBy((g) => {
+      return g.quantity > 5 ? 'ok' : 'restock';
+    });
+
+    expect(groupedByType.ok).toEqual([
+      { name: 'goat', type: 'meat', quantity: 23 },
+      { name: 'fish', type: 'meat', quantity: 22 }
+    ]);
+    expect(groupedByType.ok.length).toEqual(2);
+
+    expect(groupedByType.restock).toEqual([
+      { name: 'asparagus', type: 'vegetables', quantity: 5 },
+      { name: 'bananas', type: 'fruit', quantity: 0 },
+      { name: 'cherries', type: 'fruit', quantity: 5 }
+    ]);
+    expect(groupedByType.restock.length).toEqual(3);
+  });
 });
--- src/array-prototypes/array.prototypes.ts
+++ src/array-prototypes/array.prototypes.ts
@@ -1,12 +1,13 @@
 declare global {
   interface Array<T> {
-    groupBy<K extends keyof T>(callBack: (value: T) => T[K]): Record<PropertyKey, Array<T>>;
+    groupBy<K extends keyof T>(callBack: (value: T, index: number) => T[K]): Record<PropertyKey, Array<T>>;
+    groupBy(callBack: (value: T, index: number) => PropertyKey): Record<PropertyKey, Array<T>>;
   }
 }
 
-Array.prototype.groupBy = function <T, K extends keyof T>(callBack: (value: T) => T[K]) {
-  return this.reduce((previousValue, currentValue: T) => {
-    const key = callBack(currentValue);
+Array.prototype.groupBy = function <T, K extends keyof T>(callBack: (value: T, index: number) => T[K]) {
+  return this.reduce((previousValue, currentValue: T, currentIndex: number) => {
+    const key = callBack(currentValue, currentIndex);
     const value: T[] = previousValue[key] || [];
     previousValue[key] = value.concat([currentValue]);
     return previousValue;
