b5c7c648b2c784465e537dd0c159fb07cdeda067
Henrique Ferraz <henrique.oliveira.ferraz@protonmail.com>
2022-11-27 16:06:27 -0300

    chore: prov

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,25 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'jestGlobalMocks.ts',
+    '.module.ts',
+    '<rootDir>/src/index.ts',
+    '.mock.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
